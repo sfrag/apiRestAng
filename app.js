363b6bab73b339a0fcab9b22c062a881158706d6
sfo <sfragua83@gmail.com>
2016-11-13 03:03:16 +0100

    Tutorial terminado, falta la parte donde lo adapta a angular para darle interfaz, muy recomendable hacerlo y entenderlo

--- app.js
+++ app.js
@@ -1,21 +1,49 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    methodOverride =  require("method-override");
+    methodOverride =  require("method-override"),
     mongoose = require('mongoose');
 
+// Connection to DB
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+  if(err) throw err;
+  console.log('Connected to Database');
+});
+
+// Midlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
+// Importamos modelos y controladores
+var models = require('./models/tvshow')(app, mongoose);
+var TVShowCtrl = require('./controllers/tvshows');
+
+//Ruta de ejemplo
+
 var router = express.Router();
-
-router.get('/', function(req, res){
-    res.send("Hello World");
+router.get('/', function(req, res) {
+  res.send("Hello world!");
 });
-
 app.use(router);
 
-app.listen(3000, function(){
-    console.log("Node server running on http://localhost:3000");
+// API routes
+
+var tvshows = express.Router();
+
+tvshows.route('/tvshows')
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use('/api', tvshows);
+
+// Start server
+app.listen(3000, function() {
+  console.log("Node server running on http://localhost:3000");
 });
+
